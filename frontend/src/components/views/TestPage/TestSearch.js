1e61fdf2a5c07302980c949afd87c0837b205a67
dltjgus <dltjgus@duksung.ac.kr>
2023-11-07 17:15:33 +0900

    - 도서 검색 결과 임시 리사이클러뷰 연결
    네이버 도서 API 정보
    - 도서 검색 조건: 전체, 제목, 저자, 목차, ISBN, 출판사, 출간시작일, 출간종료일, 카테고리
    => 제목 또는 ISBN 만으로 조건 제한 가능
    
    - 도서 검색 결과: 카테고리 미포함
    => res.data.items[i].link(네이버 도서 정보 사이트)에서 크롤링

--- frontend/src/components/views/TestPage/TestSearch.js
+++ frontend/src/components/views/TestPage/TestSearch.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 function TestSearchPage() {
   const [searchText, setSearchText] = useState("")
+  const [searchResults, setSearchResults] = useState([])
 
   const onSearchTextHandler = (event) => {
     setSearchText(event.currentTarget.value)
@@ -23,31 +24,41 @@ function TestSearchPage() {
       }
     })
     .then(response => {
-      const title = response.data.items[0].title
-      const author = response.data.items[0].author
-      const publisher = response.data.items[0].publisher
-
-      alert(`title: ${title}\nauthor: ${author}\npublisher: ${publisher}`)
+      setSearchResults(response.data.items)
+      alert("검색 완료")
     })
     .catch(err => {
       alert(err)
     })
   }
 
+  //검색 결과가 변경될 때 마다...
+  useEffect(() => {
+    console.log(searchResults)
+  }, [searchResults])
+
   return (
     <div style={{
-      display: 'flex', justifyContent: 'center', alignItems: 'center',
+      display: 'flex', flexDirection: 'column', alignItems: 'center',
       width: '100%', height: '100vh'
       }}>
     
-      <form style={{display: 'flex', flexDirection: 'row'}}
-        onSubmit={onSubmitHandler}  
-      >
+      <form style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', marginBottom: '10px'}} onSubmit={onSubmitHandler}>
         <input type="text" value={searchText} onChange={onSearchTextHandler} />
         <button>
           Search
         </button>
       </form>
+
+      <div className="recyclerView">
+        {searchResults.map((result, index) => (
+          <div key={index} className="item" style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+            <img style={{height: '100px', margin: '10px'}} src={result.image}/>
+            <h4>{result.title}</h4>
+            <p style={{margin: '0px 10px'}}>작가 | {result.author}</p>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
