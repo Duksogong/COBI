d69e42c741c38afdef936d8050b2d5329538fbf4
twosky0202 <leehaneul0202@naver.com>
2023-11-23 02:44:46 +0900

    refactor : index 파일에서 reviewRoutes 분리

--- backend/index.js
+++ backend/index.js
@@ -1,69 +1,342 @@
 const express = require("express");
 const app = express();
 const port = 5000;
-
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-
-const { Review } = require("./models/Review");
-const { Image } = require("./models/Image");
-
-// 이미지 업로드를 위한 Multer 및 Memory Storage 설정
-const multer = require("multer");
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-// const imageRouter = Router();
 const config = require("./config/key");
 
+//bodyParser
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// application/json
 app.use(bodyParser.json());
-app.use(cookieParser());
 
+//mongoose 연결
 const mongoose = require("mongoose");
-
 mongoose
     .connect(config.mongoURI)
-    .then(() => console.log("MongoDB Connected.."))
+    .then(() => console.log("MongoDB Connected..."))
     .catch((err) => console.log(err));
 
-app.post("/api/review", upload.array("images", 3), async (req, res) => {
-    // const user = req.user;
-    const { user, category } = req.body; // 임시
+//router 마운트
+const searchRoutes = require("./routes/searchRoutes");
+app.use("/api/search", searchRoutes);
 
-    try {
-        const imageIds = await Promise.all(
-            req.files.map(async (file) => {
-                const image = new Image({
-                    data: file.buffer,
-                    contentType: file.mimetype,
-                });
-                const savedImage = await image.save();
-                return savedImage;
-            })
-        );
+const reviewRoutes = require("./routes/reviewRoutes");
+app.use("/api/review", reviewRoutes);
 
-        const review = new Review({
-            ...req.body,
-            // user: user._id,
-            user,
-            category,
-            images: imageIds,
-        });
-
-        const reviewInfo = await review.save();
-
-        return res.status(200).json({
-            success: true,
-            review: reviewInfo,
-        });
-    } catch (err) {
-        return res.json({
-            success: false,
-            error: err.message,
-        });
-    }
+//서버 실행
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`);
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const { User } = require("./models/User");
+const { UserCategory } = require("./models/UserCategory");
+const { Bookmark } = require("./models/Bookmark");
+
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
+const { auth } = require("./middleware/auth");
+
+//===============================================================================
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.get("/api/axios", (req, res) => {
+    res.send("AXIOS testing success");
+});
+
+app.post("/api/users/register", (req, res) => {
+    const { email, password, confirmPassword } = req.body;
+
+    user.save()
+        .then(() => {
+            // 사용자 등록 성공
+            res.status(200).json({
+                success: true,
+                message: "사용자 등록 성공",
+            });
+        })
+        .catch((err) => {
+            // 오류 처리
+            res.status(500).json({
+                success: false,
+                message: err.message,
+                error: err,
+            });
+        });
+});
+
+app.post("/api/users/login", (req, res) => {
+    User.findOne({ email: req.body.email })
+        .then((user) => {
+            if (!user) {
+                return res.json({
+                    loginSuccess: false,
+                    message: "제공된 이메일에 해당하는 유저가 없습니다.",
+                });
+            }
+            user.comparePassword(req.body.password, (err, isMatch) => {
+                if (!isMatch) {
+                    return res.json({
+                        loginSuccess: false,
+                        message: "비밀번호가 틀렸습니다.",
+                    });
+                }
+                user.generateToken((err, user) => {
+                    if (err) {
+                        return res.status(400).send(err);
+                    }
+                    res.cookie("x_auth", user.token)
+                        .status(200)
+                        .json({ loginSuccess: true, _id: user._id });
+                });
+            });
+        })
+        .catch((err) => {
+            return res.status(400).send(err);
+        });
+});
+
+app.get("/api/users/auth", auth, (req, res) => {
+    res.status(200).json({
+        _id: req.user._id,
+        email: req.user.email,
+        nickname: req.user.nickname,
+        password: req.user.password,
+        profile_image: req.user.profile_image,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+    });
+});
+
+app.get("/api/users/logout", auth, (req, res) => {
+    User.findOneAndUpdate({ _id: req.user._id }, { token: "" })
+        .then(() => {
+            res.status(200).send({
+                success: true,
+            });
+        })
+        .catch((err) => {
+            res.json({ success: false, err });
+        });
+});
+
+app.post("/api/users/reset_password", auth, (req, res) => {
+    const { password, newPW1, newPW2 } = req.body;
+
+    User.findOne({ _id: req.user._id })
+        .then((user) => {
+            if (!user) {
+                return res.json({
+                    findSuccess: false,
+                    message: "현재 유저 정보가 일치하지 않습니다.",
+                });
+            }
+
+            user.comparePassword(password, (err, isMatch) => {
+                if (!isMatch) {
+                    return res.json({
+                        compareSuccess: false,
+                        message: "현재 유저 정보가 일치하지 않습니다.",
+                    });
+                }
+
+                if (password !== newPW1) {
+                    if (newPW1.length >= 8) {
+                        if (newPW1 === newPW2) {
+                            user.generateHash(newPW2, (err, hash) => {
+                                if (!hash) {
+                                    return res.json({
+                                        hashSuccess: false,
+                                        message: "hash가 생성되지 않았습니다.",
+                                    });
+                                }
+                                User.findOneAndUpdate(
+                                    { _id: req.user._id },
+                                    { password: hash }
+                                )
+                                    .then(() => {
+                                        res.status(200).json({
+                                            hashSuccess: true,
+                                            message: "비밀번호 변경 성공",
+                                        });
+                                    })
+                                    .catch((err) => {
+                                        res.json({
+                                            hashSuccess: false,
+                                            message: "비밀번호 변경 실패",
+                                            error: err,
+                                        });
+                                    });
+                            });
+                        } else {
+                            return res.json({
+                                ifSuccess: false,
+                                message: "pw1과 pw2이 다릅니다",
+                            });
+                        }
+                    } else {
+                        return res.json({
+                            ifSuccess: false,
+                            message: "8보다 크게 설정해주세요",
+                        });
+                    }
+                } else {
+                    return res.json({
+                        ifSuccess: false,
+                        message:
+                            "새 비밀번호가 현재 비밀번호와 같으면 안됩니다",
+                    });
+                }
+            });
+        })
+        .catch((err) => {
+            return res.status(400).json({
+                findSuccess: false,
+                message: "User 접근에 실패했습니다.",
+                error: err,
+            });
+        });
+});
+
+app.post("/api/users/reset_nickname", auth, (req, res) => {
+    const newNickname = req.body.nickname;
+
+    // 닉네임이 이미 존재하는지 확인
+    User.findOne({ nickname: newNickname })
+        .then((existingUser) => {
+            if (existingUser) {
+                // 이미 존재하는 경우 오류 반환
+                return res.json({
+                    success: false,
+                    message: "이미 있는 nickname입니다.",
+                });
+            }
+
+            // 닉네임이 존재하지 않는 경우, 현재 사용자의 닉네임을 업데이트
+            User.findOneAndUpdate(
+                { _id: req.user._id },
+                { nickname: newNickname },
+                { new: true } // 업데이트된 문서를 반환하도록 설정
+            )
+                .then((updatedUser) => {
+                    if (!updatedUser) {
+                        return res.json({
+                            success: false,
+                            message: "닉네임 변경 실패",
+                        });
+                    }
+                    res.status(200).json({
+                        success: true,
+                        message: "닉네임 변경 성공",
+                    });
+                })
+                .catch((err) => {
+                    res.json({
+                        success: false,
+                        message: "닉네임 변경 실패",
+                        error: err,
+                    });
+                });
+        })
+        .catch((err) => {
+            res.json({
+                findSuccess: false,
+                message: "User 접근에 실패했습니다.",
+                error: err,
+            });
+        });
+});
+
+app.post("/api/users/select_category", auth, (req, res) => {
+    const { userId, categoryId } = req.body;
+
+    const userCategory = new UserCategory({
+        userId: userId,
+        categoryId: categoryId,
+    });
+
+    userCategory
+        .save()
+        .then(() => {
+            res.status(200).json({
+                success: true,
+                message: "Category selected successfully.",
+            });
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(500).json({
+                success: false,
+                message: "Internal server error.",
+            });
+        });
+});
+
+app.post("/api/users/deselect_category", auth, (req, res) => {
+    const { userId, categoryId } = req.body;
+
+    UserCategory.findOneAndDelete({ userId: userId, categoryId: categoryId })
+        .then(() => {
+            res.status(200).json({
+                success: true,
+                message: "Category deselected successfully.",
+            });
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(500).json({
+                success: false,
+                message: "Internal server error.",
+            });
+        });
+});
+
+app.post("/api/users/select_bookmark", auth, (req, res) => {
+    const { userId, reviewId } = req.body;
+
+    const bookmark = new Bookmark({
+        userId: userId,
+        reviewId: reviewId,
+    });
+
+    bookmark
+        .save()
+        .then(() => {
+            res.status(200).json({
+                success: true,
+                message: "Bookmark selected successfully.",
+            });
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(500).json({
+                success: false,
+                message: "Internal server error.",
+            });
+        });
+});
+
+app.post("/api/users/deselect_bookmark", auth, (req, res) => {
+    const { userId, reviewId } = req.body;
+
+    Bookmark.findOneAndDelete({ userId: userId, reviewId: reviewId })
+        .then(() => {
+            res.status(200).json({
+                success: true,
+                message: "Bookmark deselected successfully.",
+            });
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(500).json({
+                success: false,
+                message: "Internal server error.",
+            });
+        });
+});
+
+//===============================================================================
+
+app.listen(port, () => console.log(`Exmaple app listening on port ${port}!`));
