8458333526570ef37259f0f5abc212fd4dbace7f
dltjgus <dltjgus@duksung.ac.kr>
2023-11-22 21:30:47 +0900

    Feat - 책 isbn으로 감상평 조회

--- frontend/src/components/views/TestPage/TestSearchReviews.js
+++ frontend/src/components/views/TestPage/TestSearchReviews.js
@@ -1,15 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { useLocation } from "react-router-dom"
 
 function TestSearchReviews() {
   const { state } = useLocation()
+  
+  const [reviewList, setReviewList] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.post('/api/search/review', {
+        "isbn": state.isbn
+      })
+      setReviewList(response.data)
+    }
+
+    fetchData()
+  }, [state.isbn])
 
   return (  
-    <div className="item" style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
-      <img style={{height: '100px', margin: '10px'}} src={state.image}/>
-      <h4>{state.title}</h4>
-      <p style={{margin: '0px 10px'}}>작가 | {state.author}</p>
+    <div style={{
+      display: 'flex', flexDirection: 'column', alignItems: 'center',
+      width: '100%', height: '100vh'
+      }}>
+    
+      <div className="item" style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+        <img style={{height: '100px', margin: '10px'}} src={state.image}/>
+        <h4>{state.title}</h4>
+        <p style={{margin: '0px 10px'}}>작가 | {state.author}</p>
+      </div>
+
+      <div className="recyclerView">
+        {reviewList.map((result, index) => (
+          <div key={index} className="item" style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+            <h4>{result.title}</h4>
+            <p style={{margin: '0px 10px'}}>{result.review}</p>
+          </div>
+        ))}
+      </div>
     </div>
+
+    
   )
 }
 
