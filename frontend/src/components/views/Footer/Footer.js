85b3364f89158e6c075b68303cdd4ec63a5d36fe
edle607 <govldnflwlq@naver.com>
2023-12-02 10:08:00 +0900

    fix - 내책장 > 내감상평 (이름 수정)

--- frontend/src/components/views/Footer/Footer.js
+++ frontend/src/components/views/Footer/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
     <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
       <Container>
           <Nav.Link href="/home">홈</Nav.Link>
-          <Nav.Link href="#pricing">내책장</Nav.Link>
+          <Nav.Link href="#pricing">내감상평</Nav.Link>
           <Nav.Link href="/my_bookmark">책갈피</Nav.Link>
           <Nav.Link href="#features">글쓰기</Nav.Link>
           <Nav.Link href="/change_category">설정</Nav.Link>
