93b9014828d1e99a46310c59bf8cb3ec6782ce4f
twosky0202 <leehaneul0202@naver.com>
2023-12-02 16:29:28 +0900

    feat : 리뷰 페이지 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,36 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import TestSearch from './components/views/TestPage/TestSearch';
+import TestSearch from "./components/views/TestPage/TestSearch";
 import TestSearchReviews from "./components/views/TestPage/TestSearchReviews";
 
-import LandingPage from './components/views/LandingPage/LandingPage';
-import LoginPage from './components/views/LoginPage/LoginPage';
-import RegisterPage from './components/views/RegisterPage/RegisterPage';
-import ChangeNicknamePage from './components/views/ChangePage/ChangeNicknamePage';
+import LandingPage from "./components/views/LandingPage/LandingPage";
+import LoginPage from "./components/views/LoginPage/LoginPage";
+import RegisterPage from "./components/views/RegisterPage/RegisterPage";
+import ChangeNicknamePage from "./components/views/ChangePage/ChangeNicknamePage";
+import ReviewPage from "./components/views/ReviewPage/ReviewPage";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/test/search" Component={TestSearch} />
-        <Route exact path="/test/searchReviews" Component={TestSearchReviews} />
-          
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-
-        <Route path="/change_nickname" element={<ChangeNicknamePage />} />"
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route exact path="/test/search" Component={TestSearch} />
+                <Route
+                    exact
+                    path="/test/searchReviews"
+                    Component={TestSearchReviews}
+                />
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route
+                    path="/change_nickname"
+                    element={<ChangeNicknamePage />}
+                />
+                <Route path="/review" element={<ReviewPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
